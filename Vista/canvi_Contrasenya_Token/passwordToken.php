9918512d180e371821d9762b60e2836d24099dfe
MarkAlvarezCaballero <markalvarezcaballero@gmail.com>
2024-12-03 19:31:57 +0100

    Correcions Token

--- Vista/canvi_Contrasenya_Token/passwordToken.php
+++ Vista/canvi_Contrasenya_Token/passwordToken.php
@@ -6,7 +6,7 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 
 include('../header/header.php');
-
+require_once '../../Controlador/verificarPasswordToken.php';
 
 if (isset($_GET['token'])) {
     $token = $_GET['token'];
